4b48196419f0e40ab608db8ac8bf40b8dde4d9a5
gkgaurav31 <gkgaurav31@gmail.com>
2022-02-14 13:41:23 +0530

    test

---
+++ routes/test.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+var pdf = require("pdf-creator-node");
+var fs = require("fs");
+var path = require("path");
+var html = fs.readFileSync(path.join(__dirname, "../template.html"), "utf8");
+
+var options = {
+    format: "A3",
+    orientation: "portrait",
+    border: "10mm",
+  };
+  
+const orders = [
+{
+    order: 1,
+    type: "Address",
+    address: "Pune, Maharashtra",
+},
+{
+    order: 2,
+    type: "item",
+    description: "Item 1",
+    price: "30",
+},
+{
+    order: 3,
+    type: "Address",
+    address: "Pune, Maharashtra",
+},
+];
+
+var document = {
+html: html,
+data: {
+    orders,
+},
+path: "./output.pdf",
+type: "", // "stream" || "buffer" || "" ("" defaults to pdf)
+};
+
+
+router.get('/', function(req, res, next) {
+    console.log(document);
+    pdf
+    .create(document, options)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+  res.send('done');
+});
+
+module.exports = router;
